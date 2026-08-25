9da784b906d9a7bf47cd85c6278b7d9a01e1bb5c
Cuong <cuongpham2107@gmail.com>
2026-08-25 20:36:47 +0700

    fix: khôi phục hoàn toàn 100% code DriverNotificationService và notifications về commit 8b30851

--- app/Services/Notification/DriverNotificationService.php
+++ app/Services/Notification/DriverNotificationService.php
@@ -208,29 +208,16 @@ class DriverNotificationService
                 ->withData($stringData)
                 ->withAndroidConfig(AndroidConfig::fromArray([
                     'priority' => 'high',
-                    'ttl' => '0s',
                     'notification' => [
                         'sound' => 'default',
                         'channel_id' => 'orders',
-                        'priority' => 'max',
-                        'default_vibrate_timings' => true,
-                        'default_sound' => true,
                     ],
                 ]))
                 ->withApnsConfig(ApnsConfig::fromArray([
-                    'headers' => [
-                        'apns-priority' => '10',
-                        'apns-push-type' => 'alert',
-                    ],
                     'payload' => [
                         'aps' => [
-                            'alert' => [
-                                'title' => $title,
-                                'body' => $body,
-                            ],
                             'sound' => 'default',
                             'badge' => 1,
-                            'content-available' => 1,
                         ],
                     ],
                 ]));
